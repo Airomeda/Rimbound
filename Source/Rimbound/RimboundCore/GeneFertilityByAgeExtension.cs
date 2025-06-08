d003ca01eb7192a464b6e913ac0612e41a28bf06
Airo <59371341+Airomeda@users.noreply.github.com>
2025-06-08 17:51:53 +0800

    initial update.

---
+++ Source/Rimbound/RimboundCore/GeneFertilityByAgeExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace RimboundCore
+{
+    public class GeneFertilityByAgeExtension : DefModExtension
+    {
+        public SimpleCurve maleFertilityAgeFactor;
+
+        public SimpleCurve femaleFertilityAgeFactor;
+
+        public SimpleCurve fertilityAgeFactor;
+    }
+}
