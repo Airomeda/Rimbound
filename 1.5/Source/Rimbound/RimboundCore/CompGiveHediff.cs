1119c0a188ebfe302f7b2e2625407bc33e2bef14
Airo <59371341+Airomeda@users.noreply.github.com>
2025-07-12 14:55:05 +0800

    1.6 update.

---
+++ 1.5/Source/Rimbound/RimboundCore/CompGiveHediff.cs
@@ -0,0 +1,40 @@
+﻿using RimWorld;
+using Verse;
+
+namespace RimboundCore
+{
+    public class CompGiveHediff : CompAbilityEffect
+    {
+        public new CompProperties_GiveHediff Props => (CompProperties_GiveHediff)props;
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+
+            if (Props.hediffDef != null)
+            {
+                Pawn casterPawn = parent.pawn;
+                Pawn targetPawn = target.Pawn;
+
+                if (Props.applyToCaster)
+                {
+                    casterPawn.health.AddHediff(Props.hediffDef);
+                }
+                if (targetPawn != null)
+                {
+                    targetPawn.health.AddHediff(Props.hediffDef);
+                }
+                if (Props.applyToRadius)
+                {
+                    foreach (Pawn affectedPawn in casterPawn.Map.mapPawns.AllPawnsSpawned)
+                    {
+                        if (affectedPawn.Spawned && affectedPawn.Position.InHorDistOf(target.Cell, parent.def.EffectRadius))
+                        {
+                            affectedPawn.health.AddHediff(Props.hediffDef);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
